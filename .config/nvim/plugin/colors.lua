83f9b5d25cec35d65d5e626e75a7ba4619a0d388
TheSillyCoder <sarkardebayan45@gmail.com>
2023-05-04 22:19:14 +0530

    changed nvim color settings

--- .config/nvim/plugin/colors.lua
+++ .config/nvim/plugin/colors.lua
@@ -1,3 +1,7 @@
-vim.cmd[[let g:PaperColor_Theme_Options = {'theme':{'default.dark':{'transparent_background':1}}}]]
-vim.cmd[[colorscheme PaperColor]]
+-- vim.cmd[[let g:PaperColor_Theme_Options = {'theme':{'default.dark':{'transparent_background':1}}}]]
+-- vim.cmd[[colorscheme PaperColor]]
 -- vim.cmd('colorscheme rose-pine')
+vim.cmd('colorscheme onedark')
+
+vim.cmd[[highlight Normal ctermbg=none]]
+vim.cmd[[highlight NonText ctermbg=none]]
