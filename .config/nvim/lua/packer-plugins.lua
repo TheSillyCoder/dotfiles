9d66dd4ec98d5e34f53096fdb4e1ba9844025528
TheSillyCoder <sarkardebayan45@gmail.com>
2023-08-11 20:04:28 +0530

    new

--- .config/nvim/lua/packer-plugins.lua
+++ .config/nvim/lua/packer-plugins.lua
@@ -1,7 +1,7 @@
 return require('packer').startup(function()
 	use 'wbthomason/packer.nvim'
 
-	-- Stuff I genrally use 
+	-- Stuff I genrally use
 	use 'preservim/nerdtree'
 	use 'tpope/vim-commentary'
 	use 'vim-airline/vim-airline'
@@ -16,35 +16,29 @@ return require('packer').startup(function()
 	-- For LaTeX
 	use 'lervag/vimtex'
 
-	-- -- Language Server
-	-- use 'neovim/nvim-lspconfig'
-
-	-- -- Autocompletion
-	-- use 'hrsh7th/cmp-nvim-lsp'
-	-- use 'hrsh7th/cmp-buffer'
-	-- use 'hrsh7th/cmp-path'
-	-- use 'hrsh7th/cmp-cmdline'
-	-- use 'hrsh7th/nvim-cmp'
-	-- -- use 'hrsh7th/cmp-vsnip'
-	-- -- use 'hrsh7th/vim-vsnip'
-        -- use 'SirVer/ultisnips'
-        -- use 'quangnguyen30192/cmp-nvim-ultisnips'
-
-	-- Some default snippets
-	-- use 'honza/vim-snippets'
+	-- Treesitter
 	use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v2.x',
-	  requires = {
-	    -- LSP Support
-	    {'neovim/nvim-lspconfig'},             -- Required
-	    {'williamboman/mason.nvim'},           -- Optional
-	    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+		'nvim-treesitter/nvim-treesitter',
+		run = function()
+			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+			ts_update()
+		end,
+	}
 
-	    -- Autocompletion
-	    {'hrsh7th/nvim-cmp'},     -- Required
-	    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-	    {'L3MON4D3/LuaSnip'},     -- Required
-  }
-}
+	-- LSPs and Autocompletion config
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
+			-- LSP Support
+			{ 'neovim/nvim-lspconfig' }, -- Required
+			{ 'williamboman/mason.nvim' }, -- Optional
+			{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
+
+			-- Autocompletion
+			{ 'hrsh7th/nvim-cmp' }, -- Required
+			{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
+			{ 'L3MON4D3/LuaSnip' }, -- Required
+		}
+	}
 end)
--- .config/nvim/plugin/lsp-zero.lua
+++ .config/nvim/plugin/lsp-zero.lua
@@ -1,32 +1,44 @@
 local lsp = require('lsp-zero').preset({
-	name='recommended'
+	name = 'recommended'
 })
 
 lsp.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp.default_keymaps({buffer = bufnr})
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp.default_keymaps({ buffer = bufnr })
 end)
+
+lsp.format_on_save({
+	servers = {
+		['lua_ls'] = { 'lua' },
+		['rust_analyzer'] = { 'rust' },
+		['pyright'] = { 'py' },
+		['html'] = { 'html', 'htm' },
+		['clangd'] = { 'c', 'cpp' },
+		['texlab'] = { 'tex' },
+	}
+})
 lsp.ensure_installed({
 	'pyright',
 	'clangd',
 	'lua_ls',
 	'texlab',
+	'html',
 })
 local cmp = require('cmp')
 local cmp_act = lsp.cmp_action()
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 cmp.setup({
 	sources = {
-		{name = 'nvim_lsp'},
+		{ name = 'nvim_lsp' },
 	},
 	mapping = {
 		['<C-Space>'] = cmp.mapping.complete(),
-		['<CR>'] = cmp.mapping.confirm({select = true}),
-        	['<C-f>'] = cmp_act.luasnip_jump_forward(),
-	        ['<C-b>'] = cmp_act.luasnip_jump_backward(),
-        	['<C-u>'] = cmp.mapping.scroll_docs(-4),
-	        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
+		['<C-f>'] = cmp_act.luasnip_jump_forward(),
+		['<C-b>'] = cmp_act.luasnip_jump_backward(),
+		['<C-u>'] = cmp.mapping.scroll_docs(-4),
+		['<C-d>'] = cmp.mapping.scroll_docs(4),
 		['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
 		['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
 	}
