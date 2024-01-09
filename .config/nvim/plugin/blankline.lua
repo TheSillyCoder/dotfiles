15a3c341e9e9968677f54f7757668011854d9ace
TheSillyCoder <sarkardebayan45@gmail.com>
2024-01-09 16:14:04 +0530

    nvim psync

--- .config/nvim/plugin/blankline.lua
+++ .config/nvim/plugin/blankline.lua
@@ -1,9 +1,5 @@
-local blankline = require("indent_blankline")
-vim.opt.list = true
-vim.opt.listchars:append "space:⋅"
+local blankline = require("ibl")
+-- vim.opt.list = true
+-- vim.opt.listchars:append "space:⋅"
 
-blankline.setup {
-    space_char_blankline = " ",
-    show_current_context = true,
-    show_current_context_start = true,
-}
+blankline.setup()
--- .config/nvim/plugin/colors.lua
+++ .config/nvim/plugin/colors.lua
@@ -1,3 +1,6 @@
 -- vim.cmd.colorscheme("material-darker")
 vim.opt.termguicolors = true
 vim.cmd.colorscheme("ayu")
+-- vim.cmd.colorscheme("PaperColor")
+-- vim.cmd [[highlight Normal ctermbg=none]]
+-- vim.cmd [[highlight NonText ctermbg=none]]
